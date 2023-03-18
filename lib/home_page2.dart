a5e45c8a14056f3cc94dc2acdb6b6e4dd5e24422
buraktahap <05190000068@ogrenci.ege.edu.tr>
2023-03-18 05:23:22 +0300

    base home page, photo generate, save to gallery option

--- lib/home_page2.dart
+++ lib/home_page2.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -6,14 +7,14 @@ import 'package:image/image.dart' as img;
 import 'package:palette_generator/palette_generator.dart';
 import 'package:path_provider/path_provider.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MyHomePage2 extends StatefulWidget {
+  const MyHomePage2({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage2> createState() => _MyHomePage2State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePage2State extends State<MyHomePage2> {
   File? _firstImage;
   File? _secondImage;
   PaletteGenerator? _palette1;
@@ -21,6 +22,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final ImagePicker _picker = ImagePicker();
   Uint8List? _generatedImageData;
   bool _isLoading = false;
+  late List<List<List<int>>> _colorMap;
 
   Future<void> _pickFirstImage() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
@@ -58,6 +60,9 @@ class _MyHomePageState extends State<MyHomePage> {
         } else {
           _palette2 = paletteGenerator;
         }
+        if (_palette1 != null && _palette2 != null) {
+          _colorMap = _computeColorMap(_palette1!, _palette2!);
+        }
       });
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -70,54 +75,68 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Color _findNearestColor(Color color, List<Color> colorPalette) {
-    Color nearestColor = colorPalette[0];
-    double minDistance = double.infinity;
-
-    for (final paletteColor in colorPalette) {
-      final distance = _colorDistance(color, paletteColor);
-      if (distance < minDistance) {
-        minDistance = distance;
-        nearestColor = paletteColor;
-      }
-    }
-    return nearestColor;
+// Computes the Euclidean distance between two colors in Lab color space.
+  double _colorDistance(List<int> color1, List<int> color2) {
+    final lDiff = color1[0] - color2[0];
+    final aDiff = color1[1] - color2[1];
+    final bDiff = color1[2] - color2[2];
+    return sqrt(lDiff * lDiff + aDiff * aDiff + bDiff * bDiff);
   }
 
-  img.Image recolorImage(
-      img.Image source, PaletteGenerator palette1, PaletteGenerator palette2) {
-    img.Image newImage = img.Image.from(source);
-
+  List<List<List<int>>> _computeColorMap(
+      PaletteGenerator palette1, PaletteGenerator palette2) {
     final colors1 = palette1.colors.toList();
     final colors2 = palette2.colors.toList();
 
-    // Cache for nearest colors and their replacements
-    Map<Color, Color> colorCache = {};
+    final colorMap = List.generate(
+      256,
+      (i) => List.generate(
+        256,
+        (j) => List.filled(256, 0),
+      ),
+    );
 
-    for (int y = 0; y < newImage.height; y++) {
-      for (int x = 0; x < newImage.width; x++) {
-        final pixelColor = Color(newImage.getPixel(x, y));
+    for (int r = 0; r < 256; r++) {
+      for (int g = 0; g < 256; g++) {
+        for (int b = 0; b < 256; b++) {
+          int closestColorIndex = 0;
+          double closestDistance = double.infinity;
 
-        // If the color is not in the cache, find the nearest color and its replacement
-        if (!colorCache.containsKey(pixelColor)) {
-          final nearestColor = _findNearestColor(pixelColor, colors2);
-          final replacementColor = _findNearestColor(nearestColor, colors1);
-          colorCache[pixelColor] = replacementColor;
+          for (int i = 0; i < colors1.length; i++) {
+            final distance = _colorDistance(
+                _rgbToLab(colors1[i]), _rgbToLab(Color.fromARGB(255, r, g, b)));
+            if (distance < closestDistance) {
+              closestDistance = distance;
+              closestColorIndex = i;
+            }
+          }
+
+          final closestColor = colors1[closestColorIndex];
+          colorMap[r][g][b] = img.getColor(
+              closestColor.red, closestColor.green, closestColor.blue);
         }
+      }
+    }
 
-        // Set the pixel color to the cached replacement color
-        newImage.setPixel(x, y, colorCache[pixelColor]!.value);
+    return colorMap;
+  }
+
+  img.Image _remapColors(img.Image source, List<List<List<int>>> colorMap) {
+    final newImage = img.Image(source.width, source.height);
+
+// Remap the colors of each pixel in the source image to the closest
+// color in the target palette.
+    for (int y = 0; y < source.height; y++) {
+      for (int x = 0; x < source.width; x++) {
+        final pixelColor = source.getPixel(x, y);
+        final r = img.getRed(pixelColor);
+        final g = img.getGreen(pixelColor);
+        final b = img.getBlue(pixelColor);
+        newImage.setPixel(x, y, colorMap[r][g][b]);
       }
     }
 
     return newImage;
-  }
-
-  double _colorDistance(Color color1, Color color2) {
-    int dr = color1.red - color2.red;
-    int dg = color1.green - color2.green;
-    int db = color1.blue - color2.blue;
-    return (dr * dr + dg * dg + db * db).toDouble();
   }
 
   Future<void> _generateNewImage() async {
@@ -136,20 +155,15 @@ class _MyHomePageState extends State<MyHomePage> {
         _isLoading = true;
       });
 
-      // Load the first and second images
-      // final image1 = img.decodeImage(await _firstImage!.readAsBytes());
       final image2 = img.decodeImage(await _secondImage!.readAsBytes());
 
-// Perform the palette-based recoloring
-      final transformedImage = recolorImage(image2!, _palette1!, _palette2!);
+      final transformedImage = _remapColors(image2!, _colorMap);
 
-// Save the modified image locally
       final directory = await getApplicationDocumentsDirectory();
       final generatedImagePath = '${directory.path}/generated_image.png';
       File(generatedImagePath)
           .writeAsBytesSync(img.encodePng(transformedImage));
 
-// Display the generated image
       setState(() {
         _generatedImageData =
             Uint8List.fromList(img.encodePng(transformedImage));
@@ -170,7 +184,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return _isLoading == true
         ? const Padding(
             padding: EdgeInsets.all(8.0),
-            child: CircularProgressIndicator(),
+            child: Center(child: CircularProgressIndicator()),
           )
         : SingleChildScrollView(
             child: Column(
@@ -227,5 +241,37 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           );
+  }
+
+// Converts an RGB color to a Lab color.
+  List<int> _rgbToLab(Color color) {
+    final r = color.red / 255.0;
+    final g = color.green / 255.0;
+    final b = color.blue / 255.0;
+
+    final x = 0.4124 * r + 0.3576 * g + 0.1805 * b;
+    final y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    final z = 0.0193 * r + 0.1192 * g + 0.9505 * b;
+
+    const xRef = 0.95047;
+    const yRef = 1.0;
+    const zRef = 1.08883;
+
+    const epsilon = 0.008856;
+    const kappa = 903.3;
+
+    final xr = x / xRef;
+    final yr = y / yRef;
+    final zr = z / zRef;
+
+    final fx = xr > epsilon ? pow(xr, 1 / 3) : (kappa * xr + 16) / 116;
+    final fy = yr > epsilon ? pow(yr, 1 / 3) : (kappa * yr + 16) / 116;
+    final fz = zr > epsilon ? pow(zr, 1 / 3) : (kappa * zr + 16) / 116;
+
+    final l = 116 * fy - 16;
+    final a = 500 * (fx - fy);
+    final c = 200 * (fy - fz);
+
+    return [l, a, c].map((e) => e.toInt()).toList();
   }
 }
